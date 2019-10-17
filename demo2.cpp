18d51d4ddb6dc97f09ce248c8ba06ee693965d9d
James Knight <james.p.knight2@gmail.com>
2019-10-17 14:00:00 -0600

    Update demo2.cpp

--- demo2.cpp
+++ demo2.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
-
+auto above(int avg) {
+    return [avg](int x){ return x > avg; };
+}
 int main() {
-        std::vector<int> intList {};
-        int x {0};
-        std::cout << "Enter integers, enter -1 when finished\n";
-        auto fitsConstraint = [](auto num){ return (num > 0 && num < 99); };
-        while( std::cin >> x && x != -1 ) {
-                if(fitsConstraint(x))
-                        intList.push_back(x);
-        }
-        auto avg { std::accumulate(intList.begin(),intList.end(),0) / intList.size() };
-        auto aboveAvg = [avg](auto num){return num > avg;};
-        int numbersAbove {0};
-        for(auto num : intList) {
-                if (aboveAvg(num))
-                        ++numbersAbove;
-        }
-        std::cout << "There are " << numbersAbove << " numbers above the average\n";
+	std::vector<int> intList {};
+	int x {0};
+	std::cout << "Enter integers, enter -1 when finished\n";
+	auto fitsConstraint = [](auto num){ return (num > 0 && num < 100); };
+	while( std::cin >> x && x != -1 ) {
+		if(fitsConstraint(x))
+			intList.push_back(x);
+	}
+	auto avg { std::accumulate(intList.begin(),intList.end(),0) / intList.size() };
+	auto above_avg { std::count_if(intList.begin(), intList.end(), above(avg)) };
+	std::cout << above_avg << " scores above the average\n";
 return 0;
 }
