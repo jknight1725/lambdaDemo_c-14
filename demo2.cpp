02998920550f54235c249d401796f01f29258fad
James Knight <james.p.knight2@gmail.com>
2019-10-24 22:31:04 -0600

    Update demo2.cpp

--- demo2.cpp
+++ demo2.cpp
@@ -9,13 +9,13 @@ int main() {
     std::vector<double> test_scores {};
     double test_score {0};
     std::cout << "Enter test scores, enter -1 when finished\n";
-    auto fits_constraint = [](double score){ return (score > 0 && score < 100); };
+    auto fits_constraint = [](double score){ return (score >= 0 && score <= 100); };
     while( std::cin >> test_score && test_score != -1 ) {
         if(fits_constraint(test_score))
             test_scores.push_back(test_score);
     }
-    auto avg { std::reduce(test_scores.begin(), test_scores.end()) / test_scores.size() };
-    auto test_scores_above_avg { std::count_if(test_scores.begin(), test_scores.end(), test_score_above(avg)) };
-    std::cout << test_scores_above_avg << " scores above the average\n";
+    auto avg_score { std::reduce(test_scores.begin(), test_scores.end()) / test_scores.size() };
+    auto test_scores_above_avg { std::count_if(test_scores.begin(), test_scores.end(), test_score_above(avg_score)) };
+    std::cout << test_scores_above_avg << " scores above the average of " << avg_score << "\n";
     return 0;
 }
